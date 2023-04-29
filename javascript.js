679c2debfcea18ce900fc5a971d96b58b5ca7c38
Harish <89344472+harishchan2010@users.noreply.github.com>
2023-04-29 23:21:32 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ function login() {
     var password = document.getElementById("password").value;
     
 
-    // Check if the username and password match a specific user
     if (username == "kr.admin@krstudio.in" && password == "Rockin") {
         document.getElementById("content1").style.display = "block";
         document.getElementById("login").style.display = "none"
@@ -18,9 +17,7 @@ function logout1(){
     document.getElementById("content1").style.display = "none"
     document.getElementById("logout").style.display = "block"
 }
-function loginagin1(){
-    document.getElementById("login").style.display = "block"
-    document.getElementById("logout1").style.display = "none"
+
 }
 function hide1(){
     document.getElementById("hide1").style.display = "none"
@@ -28,11 +25,7 @@ function hide1(){
 }
 function logout2(){
     document.getElementById("content2").style.display = "none"
-    document.getElementById("logout2").style.display = "block"
-}
-function loginagin2(){
-    document.getElementById("login").style.display = "block"
-    document.getElementById("logout2").style.display = "none"
+     document.getElementById("logout2").style.display = "block"
 }
 function hide2(){
     document.getElementById("hide2").style.display = "none"
